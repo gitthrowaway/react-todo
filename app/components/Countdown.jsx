8d7dfc461cfc28a4fd34607dc65225870110b4df
al <ebayaccount888@yahoo.com.au>
2017-08-13 21:53:14 +1000

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var {Link} = require('react-router');
+
+var Countdown = React.createClass({
+  render: function() {
+    return <p>Countdown.jsx rendered</p>;
+  }
+});
+
+module.exports = Countdown;
