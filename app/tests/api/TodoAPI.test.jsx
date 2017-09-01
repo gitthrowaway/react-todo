0299f132958d5db018e712718327ecaa0eed1020
al <ebayaccount888@yahoo.com.au>
2017-09-02 00:26:44 +1000

    Completed Search functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -59,4 +59,58 @@ describe('TodoAPI',() => {
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'some text here',
+        completed: true
+      },
+      {
+        id: 2,
+        text: 'other text here 2',
+        completed: false
+      },
+      {
+        id: 3,
+        text: 'some text here 3',
+        completed: true
+      }
+    ];
+
+    it('should show all todos when showCompleted true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toEqual(3);
+    });
+
+    it('should only show one todo when showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos).toEqual([{
+        id: 2,
+        text: 'other text here 2',
+        completed: false
+      }]);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should return all if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toEqual(3);
+    });
+
+    it('should filter by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+
+      expect(filteredTodos.length).toEqual(2);
+    });
+  });
 });
